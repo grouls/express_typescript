1c39215af464203d7fb6ca7b9d4cb2a6a13050e4
GARY ROUSLTONE <gary.roulstone@repknight.com>
2022-12-07 13:43:39 +0000

    express setup with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    `);
+});
+
+app.listen('1234', () => {
+    console.log('Listening on port 1234')
+});
