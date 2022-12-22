f5c8369549b28dad045a22b9a2afbd144c330c8b
GARY ROUSLTONE <gary.roulstone@repknight.com>
2022-12-22 15:28:36 +0000

    refactor ouse routeBinder

---
+++ build/controllers/decorators/Metadatakeys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MetadataKeys = void 0;
+var MetadataKeys;
+(function (MetadataKeys) {
+    MetadataKeys["method"] = "method";
+    MetadataKeys["middleware"] = "middleware";
+    MetadataKeys["path"] = "path";
+    MetadataKeys["validator"] = "validator";
+})(MetadataKeys = exports.MetadataKeys || (exports.MetadataKeys = {}));
---
+++ src/controllers/decorators/MetadataKeys.ts
@@ -0,0 +1,6 @@
+export enum MetadataKeys {
+    method = 'method',
+    middleware = 'middleware',
+    path = 'path',
+    validator = 'validator'
+}
